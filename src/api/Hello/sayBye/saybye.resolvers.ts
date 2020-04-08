9784e95ce84ca7798223036c42bdd0d894e42c38
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-04-08 15:36:20 +0900

    04.08

---
+++ src/api/Hello/sayBye/saybye.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers ={
+   Query: {
+       saybye : () => `hey, what's up!`
+   }
+};
+
+export default resolvers;
---
+++ src/api/Hello/sayHello/sayhello.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers ={
+   Query: {
+       sayhello : () => `hey, what's up!`
+   }
+};
+
+export default resolvers;
