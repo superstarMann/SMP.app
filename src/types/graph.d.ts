ec18eab58f16a034a5646dc5ea891b4877aa12e5
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-04-25 17:06:44 +0900

    04.25
    place, ride 정보까지 입력했읍

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type User {\n  id: Int!\n  password: String\n  firstName: String!\n  lastName: String!\n  FullName: String\n  PhoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  Email: String\n  verifiedEmail: Boolean!\n  age: Int\n  ProfilePhoto: String!\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createAt: String!\n  updateAt: String!\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payLoad: String!\n  key: String!\n  used: Boolean!\n  createAt: String!\n  updateAt: String!\n}\n"];
+export const typeDefs = ["type Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  adress: String!\n  isFav: Boolean!\n  createAt: String!\n  updateAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAdress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAdress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  distance: String!\n  duration: String!\n  createAt: String!\n  updateAt: String\n}\n\ntype User {\n  id: Int!\n  password: String\n  firstName: String!\n  lastName: String!\n  FullName: String\n  PhoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  Email: String\n  verifiedEmail: Boolean!\n  age: Int\n  ProfilePhoto: String!\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createAt: String!\n  updateAt: String!\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payLoad: String!\n  key: String!\n  used: Boolean!\n  createAt: String!\n  updateAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -25,6 +25,33 @@ export interface User {
   lastOrientation: number | null;
   createAt: string;
   updateAt: string;
+}
+
+export interface Place {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  adress: string;
+  isFav: boolean;
+  createAt: string;
+  updateAt: string | null;
+}
+
+export interface Ride {
+  id: number;
+  status: string;
+  pickUpAdress: string;
+  pickUpLat: number;
+  pickUpLng: number;
+  dropOffAdress: string;
+  dropOffLat: number;
+  dropOffLng: number;
+  price: number;
+  distance: string;
+  duration: string;
+  createAt: string;
+  updateAt: string | null;
 }
 
 export interface Verification {
